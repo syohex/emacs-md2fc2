b8a21f6f38a401a716c25399011ecce3b3d49c07
Syohei YOSHIDA <syohex@gmail.com>
2017-01-08 21:03:38 +0900

    Update copyright year

--- md2fc2.el
+++ md2fc2.el
@@ -1,6 +1,6 @@
 ;;; md2fc2.el --- Markdown to fc2 format -*- lexical-binding: t -*-
 
-;; Copyright (C) 2016 by Syohei YOSHIDA
+;; Copyright (C) 2017 by Syohei YOSHIDA
 
 ;; Author: Syohei YOSHIDA <syohex@gmail.com>
 ;; URL: https://github.com/syohex/emacs-md2fc2
